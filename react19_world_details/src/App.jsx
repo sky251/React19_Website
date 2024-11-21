44e07e870ca133c0b6be1ffc6388b99487059bf4
Akash Patel <patelakash56964@gmail.com>
2024-11-21 16:39:03 -0500

    second

--- react19_world_details/src/App.jsx
+++ react19_world_details/src/App.jsx
@@ -1,9 +1,41 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Country } from "./pages/Country";
+import { Home } from "./pages/Home";
 
-const App = ()=>{
-  return <>
-  <h1>Hello App</h1>
-  </>
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+    ],
+  },
+]);
 
-export default App
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
+};
+
+export default App;
